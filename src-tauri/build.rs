c614423c7e1a6dcb226316af9bdfad40cf2fb515
GalaxyShad <vadim.kadyrin@mail.ru>
2024-04-16 19:47:27 +0500

    🚧 build fix

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -4,9 +4,11 @@ fn main() {
     env::set_var("CC_ENABLE_DEBUG_OUTPUT", "1");
     env::set_var("VSLANG", "1033");
     env::set_var("RUST_LOG", "debug");
+    
 
     cc::Build::new()
         .cpp(true)
+        .flag("-std=c++11")
         .include("../core/src")
         .file("../core/src/core/cpu.cpp")
         .file("../core/src/core/cpu.instructions.cpp")

236c6d493d8e7087a45c3cea0f98806e83c58504
GalaxyShad <vadim.kadyrin@mail.ru>
2024-05-10 02:48:43 +0500

    ✨new fast and virtualized hex editor component

--- src-client-app/src/tabs/UmpkRamTab.tsx
+++ src-client-app/src/tabs/UmpkRamTab.tsx
@@ -1,26 +1,28 @@
-import { useEffect, useState } from 'react'
-import { invoke } from '@tauri-apps/api/tauri'
+import { useState } from 'react'
 import HexEditor from '@/tabs/HexEditor.tsx'
 
 
 export default function UmpkRamTab() {
-  const [memory, setMemory] = useState<Uint8Array>(new Uint8Array())
+  const [memory, setMemory] = useState<Uint8Array>(new Uint8Array(2048))
 
-  const f = async () => {
-    const umpkRam = await invoke<Uint8Array>('umpk_get_ram')
+  // const f = async () => {
+  //   const umpkRam = await invoke<Uint8Array>('umpk_get_ram')
+  //
+  //   setMemory(umpkRam)
+  // }
 
-    setMemory(umpkRam)
-  }
-
-  useEffect(() => {
-    const int = setInterval(f, 60)
-
-    return () => clearInterval(int)
-  }, [])
+  // useEffect(() => {
+  //   const int = setInterval(f, 60)
+  //
+  //   return () => clearInterval(int)
+  // }, [])
 
   return (
     <div className="flex h-full w-full">
-      <HexEditor memory={memory} startLabel={0x0800} />
+      <HexEditor memory={memory} onMemoryChange={(x, i) => setMemory(mem => {
+        mem[i] = x
+        return new Uint8Array(mem)
+      })} startLabel={0x0800} />
     </div>
   )
 }

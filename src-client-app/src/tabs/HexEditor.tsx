088484d431c3885614850e2349570f38ff1f26a4
GalaxyShad <vadim.kadyrin@mail.ru>
2024-05-11 00:31:21 +0500

    ✨RAM editing ✨paste to hex editor from clipboard

--- src-client-app/src/tabs/HexEditor.tsx
+++ src-client-app/src/tabs/HexEditor.tsx
@@ -1,81 +1,150 @@
+import { AutoSizer, Grid, GridCellRenderer } from 'react-virtualized'
 import { cn } from '@/lib/utils.ts'
-import React from 'react'
+import { ChangeEventHandler, useState } from 'react'
 
 const BYTES_PER_ROW = 16
 
 interface HexEditorCellProps {
-  byte: number
-  index: number
-  onChange?: (value: number) => void
+  value: number,
+  onChange?: (value: number) => void,
+  readonly?: boolean,
+  onPaste?: (values: Uint8Array) => void,
 }
 
-function HexEditorCell(props: HexEditorCellProps) {
-  let { byte, index, onChange } = props
+function HexEditorCell({ value, onChange, onPaste, readonly = false }: HexEditorCellProps) {
+  const [isChanging, setIsChanging] = useState(false)
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const handleByteChange = (index: number, value: string, e: React.ChangeEvent<HTMLInputElement>) => {
-    const num = parseInt(value, 16)
+  const onInputChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    if (e.target.value === '')
+      return
 
-    if (!isNaN(num) && num >= 0 && num <= 255) {
-      onChange?.(num)
+    if (e.target.value.length > 2) {
+      getSiblingDiv(e.target.parentElement, 1, 1)?.focus()
     }
 
-    if (value.length >= 2) {
-      const nextInput = e.target.parentElement?.nextElementSibling?.children[0] as HTMLInputElement
-      nextInput.focus()
+    const num = parseInt(e.target.value, 16)
+
+    if (!isNaN(num) && num >= 0 && num <= 255) {
+      e.target.value = num.toHexString()
+      onChange?.(num)
+    } else {
+      e.target.value = ''
     }
   }
 
+  const getSiblingDiv = (parentElement: Element | null | undefined, direction: number, steps: number): HTMLDivElement | undefined => {
+    let sibling = parentElement
+
+    for (let i = 0; i < steps; i++) {
+      while ((sibling = sibling?.[direction === 1 ? 'nextElementSibling' : 'previousElementSibling'])?.children?.[0].tagName === 'P') { /* empty */
+      }
+    }
+
+    return sibling?.children?.[0] as HTMLDivElement | undefined
+  }
+
+  const onKeyDown: React.KeyboardEventHandler<HTMLInputElement> = async (e) => {
+    if (e.key === 'ArrowLeft') {
+      const prevDiv = getSiblingDiv(e.currentTarget.parentElement, -1, 1)
+      prevDiv?.focus()
+    } else if (e.key === 'ArrowRight') {
+      const nextDiv = getSiblingDiv(e.currentTarget.parentElement, 1, 1)
+      nextDiv?.focus()
+    } else if (e.key === 'ArrowUp') {
+      const upDiv = getSiblingDiv(e.currentTarget.parentElement, -1, 16)
+      upDiv?.focus()
+    } else if (e.key === 'ArrowDown') {
+      const downDiv = getSiblingDiv(e.currentTarget.parentElement, 1, 16)
+      downDiv?.focus()
+    } else if (e.key === 'Backspace') {
+      if ((e.currentTarget.value === '') || (e.currentTarget.value === '00')) {
+        getSiblingDiv(e.currentTarget.parentElement, -1, 1)?.focus()
+        e.currentTarget.value = '00'
+        onChange?.(0)
+      }
+    }
+  }
+
+  const onPasteHandle: React.ClipboardEventHandler<HTMLInputElement> = (e) => {
+    const data = e.clipboardData.getData('text/plain')
+
+    if (data === '')
+      return
+
+    const arr = data.split('\n').map(x => Number.parseInt(x, 16))
+
+    e.preventDefault()
+
+    onPaste?.(new Uint8Array(arr))
+  }
+
+  const isShouldInputRender = isChanging && !readonly
+
   return (
-    <td className={cn(index === 7 && 'pl-2', (byte === 0) && 'text-neutral-700')} key={index}>
-      {byte.toHexString()}
-      {/*<input*/}
-      {/*  className={cn("w-6 bg-transparent", (byte === 0) && 'text-neutral-700')}*/}
-      {/*  type="text"*/}
-      {/*  value={hex(byte)}*/}
-      {/*  onFocus={(e) => e.target.value = ''}*/}
-      {/*  onBlur={(e) => e.target.value = hex(byte)}*/}
-      {/*  onChange={(e) => handleByteChange(index, e.target.value, e)}*/}
-      {/*/>*/}
-    </td>
+    <>
+      {!isShouldInputRender && (
+        <div tabIndex={0} onFocus={() => setIsChanging(true)}
+             className={cn((value === 0x00) && 'text-secondary', 'm-auto w-full h-full')}>
+          {value.toHexString()}
+        </div>
+      )}
+      {isShouldInputRender && (
+        <input
+          autoFocus
+          placeholder={value.toHexString()}
+          onPaste={onPasteHandle}
+          className="text-center font-mono w-full h-full bg-transparent border-b border-b-primary outline-none"
+          onBlur={() => setIsChanging(false)}
+          onKeyDown={onKeyDown}
+          onChange={onInputChange}
+        />
+      )}
+    </>
   )
 }
 
 interface HexEditorProps {
   memory: Uint8Array,
   onMemoryChange?: (value: number, index: number) => void,
-  startLabel?: number
+  startLabel?: number,
+  readonly?: boolean,
+  onPaste?: (clipboard: Uint8Array, fromIndex: number) => void,
 }
 
-export default function HexEditor({ memory, onMemoryChange, startLabel = 0 }: HexEditorProps) {
-  const renderRow = (startIndex: number) => {
-    const bytes = memory.slice(startIndex, startIndex + BYTES_PER_ROW)
+export default function HexEditor({ memory, onMemoryChange, startLabel, onPaste, readonly = false }: HexEditorProps) {
+  const colNum = BYTES_PER_ROW + 1
+  const linesCount = Math.ceil(memory.length / BYTES_PER_ROW)
+
+  const cellRenderer: GridCellRenderer = ({ columnIndex, key, rowIndex, style }) => {
+    const index = rowIndex * BYTES_PER_ROW + columnIndex
+
     return (
-      <tr key={startIndex}>
-        <td className="pr-3"> {(startLabel + startIndex).toHexString(2)}</td>
-        {Array.from(bytes).map((byte, index) => (
-          <td key={startIndex + 1 + index} className={cn(index === 8 && 'pl-2', (byte === 0) && 'text-neutral-700')}>
-            {byte.toHexString()}
-          </td>))}
-      </tr>
+      <div className={cn('font-mono text-center', columnIndex === 8 && 'pl-1')} key={key} style={style}>
+        {columnIndex !== 0 &&
+          <HexEditorCell readonly={readonly} value={memory[index - 1]}
+                         onChange={x => onMemoryChange?.(x, index - 1)}
+                         onPaste={arr => onPaste?.(arr, index - 1)}
+          />}
+        {columnIndex === 0 && <p data-type="label">{((startLabel ?? 0) + rowIndex * BYTES_PER_ROW).toHexString(2)}</p>}
+      </div>
     )
   }
 
   return (
-    <table className="font-mono">
-      <thead>
-      <tr>
-        <th>ADR</th>
-        {Array.from(Array(BYTES_PER_ROW).keys()).map((byteIndex) => (
-          <th key={byteIndex}>{byteIndex.toHexString()}</th>
-        ))}
-      </tr>
-      </thead>
-      <tbody>
-      {Array.from(Array(Math.ceil(memory.length / BYTES_PER_ROW)).keys()).map((rowIndex) =>
-        renderRow(rowIndex * BYTES_PER_ROW),
+    <AutoSizer>
+      {({ height, width }) => (
+        <Grid
+          className="font-mono"
+          height={height}
+          width={width}
+          rowCount={linesCount}
+          columnCount={colNum}
+          rowHeight={22}
+          columnWidth={({ index }) => (index === 8) ? 30 : ((index === 0) ? 64 : 24)}
+          cellRenderer={cellRenderer}
+        />
       )}
-      </tbody>
-    </table>
+    </AutoSizer>
   )
+
 }
--- src-client-app/src/tabs/UmpkRamTab.tsx
+++ src-client-app/src/tabs/UmpkRamTab.tsx
@@ -1,28 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import HexEditor from '@/tabs/HexEditor.tsx'
+import { umpkGetRAM, umpkWriteToMemory } from '@/services/umpkService.ts'
 
 
 export default function UmpkRamTab() {
   const [memory, setMemory] = useState<Uint8Array>(new Uint8Array(2048))
 
-  // const f = async () => {
-  //   const umpkRam = await invoke<Uint8Array>('umpk_get_ram')
-  //
-  //   setMemory(umpkRam)
-  // }
+  const f = async () => {
+    const umpkRam = await umpkGetRAM()
 
-  // useEffect(() => {
-  //   const int = setInterval(f, 60)
-  //
-  //   return () => clearInterval(int)
-  // }, [])
+    setMemory(umpkRam)
+  }
+
+  useEffect(() => {
+    const int = setInterval(f, 1)
+
+    return () => clearInterval(int)
+  }, [])
+
+  const onMemoryChange = async (x: number, i: number) => {
+    await umpkWriteToMemory(0x0800 + i, x)
+
+    setMemory(mem => {
+      mem[i] = x
+      return new Uint8Array(mem)
+    })
+  }
+
+  const onPaste = async (arr: Uint8Array, fromIndex: number) => {
+    await Promise.all(Array.from(arr.entries()).map(([i, x]) => umpkWriteToMemory(0x0800 + fromIndex + i, x)))
+  }
 
   return (
     <div className="flex h-full w-full">
-      <HexEditor memory={memory} onMemoryChange={(x, i) => setMemory(mem => {
-        mem[i] = x
-        return new Uint8Array(mem)
-      })} startLabel={0x0800} />
+      <HexEditor memory={memory} onPaste={onPaste} onMemoryChange={onMemoryChange} startLabel={0x0800} />
     </div>
   )
 }

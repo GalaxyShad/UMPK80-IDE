6d4b567538bb3c39f54e684b97b60209e09c14f0
Adolf <fable.chernakov@mail.ru>
2024-05-19 02:12:27 +0500

    ☺☻♥♦♣♠

--- src-client-app/src/tabs/Intel8080CommandsTab.tsx
+++ src-client-app/src/tabs/Intel8080CommandsTab.tsx
@@ -1,13 +1,37 @@
-import { docIntel8080Commands } from '@/assets/docIntel8080Commands.ts'
+// import { docIntel8080Commands } from '@/assets/docIntel8080Commands.ts'
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip.tsx'
+import { cn } from '@/lib/utils';
+
+import { default as docIntel8080Commands } from '@/assets/out.json';
+
+const GroopCommand = {
+  ["Undocumented"]: "text-zinc-400",
+  ["Misc"]: "text-purple-400",
+  ["Jumps"]: "text-orange-400",
+  ["8bit load"]: "text-blue-400",
+  ["16bit load"]: "text-green-400",
+  ["8bit arithmetic"]: "text-yellow-400",
+  ["16bit arithmetic"]: "text-pink-400",
+} as Record<string, string>
 
 export function Intel8080CommandsTab() {
   console.log({ docIntel8080Commands })
 
-  const da = Array(256).fill(0).map((_, i) => docIntel8080Commands.find(x => x.code === i) ?? {
-    code: i,
-    name: '-',
-    description: '-',
+  const da = Array(256).fill(0).map((_, i) => docIntel8080Commands.find(x => +("0x" + x.hexOpcode) === i) ?? {
+    hexOpcode: "-",
+    decimalOpcode: "",
+    binaryOpcode: "",
+    mnemonic: "-",
+    argument1: "",
+    argument2: "",
+    cycles: "",
+    mask: "",
+    flags: "",
+    tags: "",
+    bytesCount: "1",
+    flagsAffected: "",
+    functionDescription: "",
+    rusDescription: ""
   })
 
   return (
@@ -23,27 +47,33 @@ export function Intel8080CommandsTab() {
                 <div className="flip-card-inner">
                   <div className="flip-card-front">
                     <div
-                      className="border flex text-foreground/25 text-center justify-center items-center px-1 w-full h-full m-auto">
-                      {x.name}
+                      className={cn(GroopCommand[x.tags[0]], "border flex text-center justify-center items-center px-1 w-full h-full m-auto")}>
+                      {cn(x.mnemonic, [x.argument1, x.argument2].filter(y => y).join(","))}
                     </div>
                   </div>
                   <div className="flip-card-back flex justify-center items-center">
-                    {x.code.toHexString()}
+                    {x.hexOpcode}
                   </div>
                 </div>
               </div>
             </TooltipTrigger>
-            <TooltipContent alignOffset={64} align="start" className="w-48 border-primary/50">
+            <TooltipContent alignOffset={64} align="start" className="min-w-48 max-w-96 border-primary/50">
               <div className="flex flex-row justify-between">
                 <div className="mb-2 font-bold">
-                  {x.name}
+                  {cn(x.mnemonic, [x.argument1, x.argument2].filter(y => y).join(","))}
                 </div>
                 <div>
-                  {x.code.toHexString()}
+                  {x.hexOpcode}
                 </div>
               </div>
               <div>
-                {x.description}
+                {x.functionDescription}
+              </div>
+              <div>
+                {cn("Влияние на флаги: ", x.flags != "" ? x.flags : "отсутствует")}
+              </div>
+              <div>
+                {x.rusDescription}
               </div>
             </TooltipContent>
           </Tooltip>

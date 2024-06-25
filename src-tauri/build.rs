8d503a57c4b61485a0848316d772c2a705b0c094
GalaxyShad <vadim.kadyrin@mail.ru>
2024-06-25 22:28:37 +0500

    👷 SomeIntel8080ASMTranslator build integration

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,4 +1,5 @@
-use std::env;
+use std::{env};
+use std::process::Command;
 use cc::Build;
 
 fn main() {
@@ -14,6 +15,28 @@ fn main() {
         .file("../core/src/core/cpu.instructions.cpp")
         .file("../core/src/cumpk80.cpp")
         .compile("cumpk80");
+
+    let target_triple = env::var("TARGET").expect("TARGET environment variable not set");
+    let rid = env::var("RID").unwrap_or_else(|_| "win-x64".to_string());
+
+    let output = Command::new("dotnet")
+        .arg("publish")
+        .arg("../translator/SomeAsmTranslator/SomeAsmTranslator.csproj")
+        .arg("--self-contained").arg("true")
+        .arg("--runtime").arg(&rid)
+        .arg("--output").arg("./bin")
+        .arg("-p:PublishSingleFile=true")
+        .arg("-p:UseAppHost=true")
+        .arg(format!("-p:AssemblyName=i8080-{}", target_triple))
+        .output()
+        .expect("Check that .NET 8.0 installed");
+
+    if !output.status.success() {
+        let stdout = String::from_utf8_lossy(&output.stdout);
+
+        println!("cargo::warning=SomeIntel8080ASMTranslator build fail. rid={}", rid);
+        println!("cargo::warning={:?}", &stdout);
+    }
 
     println!("cargo::rerun-if-changed=../core/src/");
 
